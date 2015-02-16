49d6dd24f7f5508df864754e42002f277ceaaa2d
guspowell <augustuspowell@me.com>
2015-02-16 12:52:46 +0000

    started toy app

--- toy_app/app/controllers/application_controller.rb
+++ toy_app/app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def hello
-    render text: "goodbye world"
-  end
-
-  def goodbye
-    render text: "goodbye, world!"
-  end
-
 end
